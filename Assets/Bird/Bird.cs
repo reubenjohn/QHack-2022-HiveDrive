5d6f8b3c4cc95096c91cf16528d16ea5f123ceed
Reuben John <reubenvjohn@gmail.com>
2022-07-20 04:53:50 -0400

    Minor code cleanup

--- Assets/Bird/Bird.cs
+++ Assets/Bird/Bird.cs
@@ -1,67 +1,72 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using static Utils;
 
-public class Bird : MonoBehaviour
+namespace Bird
 {
-    [SerializeField] private float maxSpeed = 4f;
-    [SerializeField] private float personalSpaceCoefficient = 4f;
-    [SerializeField] private float alignmentCoefficient = .1f;
-    [SerializeField] private float healthSpeed = 1e-8f;
-    [SerializeField] private float explosionRadius = 1f;
-    [SerializeField] private float explosionPower = 10f;
-    public Transform front;
-    public Transform spawnPoint;
-
-    private Rigidbody2D rb;
-    private SpriteRenderer spriteRenderer;
-    private float health = 0;
-
-    void Start()
+    public class Bird : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-    void Update()
-    {
-        LerpHealth(+1);
-        rb.velocity = transform.up.normalized * maxSpeed;   // Only move in forward direction
-        spriteRenderer.color = Color.Lerp(Color.red, Color.blue, (.5f + health * 500f));
-        float random = UnityEngine.Random.Range(0, 1f);
-        if (health > random)
-            Reproduce();
-        else if (health < -random)
-            Die();
-    }
-    void Reproduce()
-    {
-        this.health = 0;
-        Instantiate(gameObject, spawnPoint.position, spawnPoint.rotation, transform.parent).GetComponent<Bird>().health = 0;
-    }
-    void Die()
-    {
-        Vector3 explosionPos = transform.position;
-        foreach (Collider2D hit in Physics2D.OverlapCircleAll(explosionPos, explosionRadius))
-            if (hit.TryGetComponent<Rigidbody2D>(out Rigidbody2D nearbyRigidBody))
-                nearbyRigidBody.AddForce((hit.transform.position - explosionPos).normalized * explosionPower, ForceMode2D.Impulse);
-        Destroy(gameObject);
-    }
+        [SerializeField] private float maxSpeed = 4f;
+        [SerializeField] private float personalSpaceCoefficient = 4f;
+        [SerializeField] private float alignmentCoefficient = .1f;
+        [SerializeField] private float healthSpeed = 1e-8f;
+        [SerializeField] private float explosionRadius = 1f;
+        [SerializeField] private float explosionPower = 10f;
+        public Transform front;
+        public Transform spawnPoint;
 
-    void OnCollisionStay2D(Collision2D other) => LerpHealth(-10);
+        private Rigidbody2D rb;
+        private SpriteRenderer spriteRenderer;
+        private float health = 0;
 
-    void OnTriggerStay2D(Collider2D other)  // obstacle in range
-    {
-        AvoidCollision(other);
-        if (other.TryGetComponent(out Bird bird))
-            TryAlignWithBird(bird);
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            LerpHealth(+1);
+            rb.velocity = transform.up.normalized * maxSpeed;   // Only move in forward direction
+            spriteRenderer.color = Color.Lerp(Color.red, Color.blue, (.5f + health * 500f));
+            float random = Random.Range(0, 1f);
+            if (health > random)
+                Reproduce();
+            else if (health < -random)
+                Die();
+        }
+
+        private void Reproduce()
+        {
+            this.health = 0;
+            Instantiate(gameObject, spawnPoint.position, spawnPoint.rotation, transform.parent).GetComponent<Bird>().health = 0;
+        }
+
+        private void Die()
+        {
+            Vector3 explosionPos = transform.position;
+            foreach (Collider2D hit in Physics2D.OverlapCircleAll(explosionPos, explosionRadius))
+                if (hit.TryGetComponent<Rigidbody2D>(out Rigidbody2D nearbyRigidBody))
+                    nearbyRigidBody.AddForce((hit.transform.position - explosionPos).normalized * explosionPower, ForceMode2D.Impulse);
+            Destroy(gameObject);
+        }
+
+        private void OnCollisionStay2D(Collision2D other) => LerpHealth(-10);
+
+        private void OnTriggerStay2D(Collider2D other)  // obstacle in range
+        {
+            AvoidCollision(other);
+            if (other.TryGetComponent(out Bird bird))
+                TryAlignWithBird(bird);
+        }
+
+        private void AvoidCollision(Collider2D other)
+        {
+            Vector2 vectorToTarget = other.ClosestPoint(front.position) - (Vector2)front.position;
+            rb.AddForceAtPosition(vectorToTarget.normalized * -personalSpaceCoefficient / (vectorToTarget.magnitude + .1f), front.position);
+        }
+
+        private void TryAlignWithBird(Bird bird) => transform.rotation = Quaternion.Slerp(transform.rotation, bird.transform.rotation, alignmentCoefficient);
+        private void LerpHealth(float target) => health = Lerp(health, target, healthSpeed);
     }
-
-    void AvoidCollision(Collider2D other)
-    {
-        Vector2 vectorToTarget = other.ClosestPoint(front.position) - (Vector2)front.position;
-        rb.AddForceAtPosition(vectorToTarget.normalized * -personalSpaceCoefficient / (vectorToTarget.magnitude + .1f), front.position);
-    }
-
-    void TryAlignWithBird(Bird bird) => transform.rotation = Quaternion.Slerp(transform.rotation, bird.transform.rotation, alignmentCoefficient);
-    void LerpHealth(float target) => health = Lerp(health, target, healthSpeed);
 }
